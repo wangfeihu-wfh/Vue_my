46dd82b99c1166a6fe859aa2c362bc2dafc226ed
wangfeihu-wfh <2456539214@qq.com>
2022-03-18 16:27:07 +0800

    feature_x

--- myshopping/src/pages/Home/index.vue
+++ myshopping/src/pages/Home/index.vue
@@ -1,36 +1,36 @@
 <template>
   <div>
-      <TypeNav/>
-      <ListContainer/>
-      <Recommend/>
-      <Rank/>
-      <Like/>
-      <Floor/>
-      <Floor/>
-      <Brand/>
+    <!-- 这是网页结构 -->
+    <TypeNav />
+    <ListContainer />
+    <Recommend />
+    <Rank />
+    <Like />
+    <Floor />
+    <Floor />
+    <Brand />
   </div>
 </template>
 
 <script>
-import ListContainer from './ListContainer/index.vue'
-import Recommend from './Recommend/index.vue'
-import Rank from './Rank/index.vue'
-import Like from './Like/index.vue'
-import Floor from './Floor/index.vue'
-import Brand from './Brand/index.vue'
+import ListContainer from "./ListContainer/index.vue";
+import Recommend from "./Recommend/index.vue";
+import Rank from "./Rank/index.vue";
+import Like from "./Like/index.vue";
+import Floor from "./Floor/index.vue";
+import Brand from "./Brand/index.vue";
 export default {
-name:'HomePage',
-components:{
+  name: "HomePage",
+  components: {
     ListContainer,
     Recommend,
     Rank,
     Like,
     Floor,
-    Brand
-}
-}
+    Brand,
+  },
+};
 </script>
 
 <style>
-
 </style>

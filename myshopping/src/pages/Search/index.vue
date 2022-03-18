2525f3a0856aae295fd21aaa4d985ffa1aeaacd3
wangfeihu-wfh <2456539214@qq.com>
2022-03-18 17:04:34 +0800

    Search index

--- myshopping/src/pages/Search/index.vue
+++ myshopping/src/pages/Search/index.vue
@@ -1,17 +1,14 @@
 <template>
   <div>
-      <!-- <h1>params参数{{$route.params.keyWord}}</h1>
-      <h1>query参数{{$route.query.k}}</h1> -->
-      <typeNav/>
+    <typeNav />
   </div>
 </template>
 
 <script>
 export default {
-name:'SearchPage'
-}
+  name: "SearchPage",
+};
 </script>
 
 <style>
-
 </style>
